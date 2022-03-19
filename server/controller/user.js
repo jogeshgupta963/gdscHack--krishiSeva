6ed0ec544325285019d0918af0c3338e96f240ff
paarth-sharma <paarthsharma2003@gmail.com>
2022-03-19 17:20:37 +0530

    added get functions for signup and login

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,4 +1,5 @@
 const user = require('../models/user')
+const path = require('path')
 
 
 async function postUser(req, res) {
@@ -15,10 +16,20 @@ async function postUser(req, res) {
 function login(req, res){
 
     try {
+
         res.status(200).json(req.body)
     } catch (err) {
         res.status(400).json(err.message)
     }
 }
 
-module.exports = { postUser }
+function getLogin(req, res){
+    // res.sendFile(path.join(__dirname,"../public/","login.html"));
+    res.sendFile(path.join(__dirname,"../../client/login.html"))
+}
+
+function getSignup(req, res){
+    res.sendFile(path.join(__dirname,"../public/","register.html"));
+}
+
+module.exports = { postUser,getLogin, getSignup }
