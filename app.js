08cb2ed87fed2603bd3d244e7dbbe959f425f0b1
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2022-03-19 19:27:30 +0530

    node added

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-const express = require('express')
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
 
 //functions
 
-const connect = require('./server/dbConnection/connect')
+const connect = require("./server/dbConnection/connect");
 const app = express();
-
 
 app.use(express.json());
 
-app.use("/css", express.static(__dirname + "/client/components/css"))
-app.use("/js", express.static(__dirname + "/client/components/js"))
-app.use("/img", express.static(__dirname + "/client/images"))
+app.use("/css", express.static(__dirname + "/client/components/css"));
+app.use("/js", express.static(__dirname + "/client/components/js"));
+app.use("/img", express.static(__dirname + "/client/images"));
 //routers
-const userRouter = require('./server/routers/user.js')
-const farmerRouter = require('./server/routers/farmer.js')
+const userRouter = require("./server/routers/user.js");
+const farmerRouter = require("./server/routers/farmer.js");
 
-app.use('/api/v1/user', userRouter)
-app.use('/api/v1/farmer', farmerRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/farmer", farmerRouter);
 
-//db connection 
-
-
+//db connection
+app.get("/api/v1/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/index.html"));
+});
 (async function () {
-    try {
-        await connect(process.env.mongoUri)
-        console.log('db connected')
-        app.listen(process.env.port || 3000, () => { console.log('server started') })
-    } catch (err) {
-        console.log(err.message)
-    }
+  try {
+    await connect(process.env.mongoUri);
+    console.log("db connected");
+    app.listen(process.env.port || 3000, () => {
+      console.log("server started");
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 })();
