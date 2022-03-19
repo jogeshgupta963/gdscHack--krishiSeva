53bfbb663226fe70f22a32ab117e8155cf2f8638
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2022-03-20 03:43:56 +0530

    added some html and css

--- server/controller/farmer.js
+++ server/controller/farmer.js
@@ -1,34 +1,31 @@
-const path = require('path');
-const farmer = require('../models/farmer');
-const user = require('../models/user');
-
+const path = require("path");
+const farmer = require("../models/farmer");
+const user = require("../models/user");
 
 async function postFarmer(req, res) {
-
-    try {
-        let userData = req.body
-        let data = await farmer.create(userData)
-        res.status(200).json(data)
-    } catch (err) {
-        res.status(400).json(err.message);
-    }
+  try {
+    let userData = req.body;
+    let data = await farmer.create(userData);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
 }
 
-
 async function getAllProfiles(req, res) {
-
-    try {
-        let allProfs = await farmer.find();
-        res.status(200).json({
-            type: "success",
-            profile: allProfs
-        })
-    } catch (error) {
-        res.status(400).json(error.message)
-    }
+  try {
+    let allProfs = await farmer.find();
+    res.status(200).json({
+      type: "success",
+      profile: allProfs,
+    });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 }
 
 function getHome(req, res) {
-    res.sendFile(path.join(__dirname, "../../client/home.html"))
+  res.sendFile(path.join(__dirname, "../../client/home.html"));
 }
-module.exports = { postFarmer, getAllProfiles, getHome }
+
+module.exports = { postFarmer, getAllProfiles, getHome };
