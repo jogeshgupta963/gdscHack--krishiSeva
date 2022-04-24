3e8810135a3e4e8fc2e1f0bf282db2a990b72096
Amit Goyal <agoyal3_be21@thapar.edu>
2022-04-24 12:45:05 +0530

    Created Register Page

--- client/src/components/Auth/Register/Register.js
+++ client/src/components/Auth/Register/Register.js
@@ -1,35 +1,78 @@
-import React from 'react'
-
+import React, { Fragment } from 'react'
+import {Link} from 'react-router-dom';
+import Navbar from '../../Navbar/Navbar';
+import './Register.css';
 function Register() {
   return (
-    <div>
-      Compiled with warnings.
+  
+    <Fragment>
+       <Navbar />
 
-src/App.js
-  Line 5:8:  'Navbar' is defined but never used  no-unused-vars
+<div className="main_container container-fluid">
+<div className="row  align-items-center">
+<div className="col-6 text-start m-0 p-0" id="main-box">
+            <div className="text_box ms-md-5 mt-md-3">
+              <h1 className="display-1 fw-bolder" id="heading">
+                Krishi<span>Seva</span>
+              </h1>
+              <h3 className="mt-3">Hello there!</h3>
+              <h3>Sign up to get started with KrishiSeva</h3>
+            </div>
 
-src/components/Home/Home.js
-  Line 46:21:   The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-  Line 46:66:   img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                                                                                                                                                                                                                                          jsx-a11y/alt-text
-  Line 74:21:   The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-  Line 74:33:   img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                                                                                                                                                                                                                                          jsx-a11y/alt-text
-  Line 102:21:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-  Line 102:33:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                                                                                                                                                                                                                                          jsx-a11y/alt-text
-
-Search for the keywords to learn more about each warning.
-To ignore, add // eslint-disable-next-line to the line before.
-
-WARNING in src/App.js
-  Line 5:8:  'Navbar' is defined but never used  no-unused-vars
-
-src/components/Home/Home.js
-  Line 46:21:   The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-  Line 46:66:   img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                                                                                                                                                                                                                                          jsx-a11y/alt-text
-  Line 74:21:   The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-  Line 74:33:   img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                                                                                                                                                                                                                                          jsx-a11y/alt-text
-  Line 102:21:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-  Line 102:33:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images     
-    </div>
+            <div className='form_box'>
+              <form>
+                <div className="form-group row ms-5 mt-5">
+                <label for="formGroupExampleInput">
+                  Full Name
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control my-3 w-50"
+                      placeholder="Your Full Name Here"
+                    />
+                  </label>
+                  <label for="formGroupExampleInput">
+                    Phone Number
+                    <input
+                      type="number"
+                      name="name"
+                      className="form-control my-3 w-50"
+                      placeholder="Your Phone Number Here"
+                    />
+                  </label>
+                  <label for="inputPassword6">
+                    Password
+                    <input
+                      type="password"
+                      name="name"
+                      className="form-control my-3 w-50"
+                      placeholder="Your Password Here"
+                    />
+                  </label>
+                  <label for="inputPassword6">
+                    Confirm Password
+                    <input
+                      type="password"
+                      name="name"
+                      className="form-control my-3 w-50"
+                      placeholder="Re-Enter your Password Here"
+                    />
+                  </label>
+                  <Link
+                    to="#"
+                    className="btn text-center text-light fw-bold mt-4 w-50 px-0 mx-2 py-2"
+                    id="signUp_btn"
+                  >
+                    Sign Up
+                  </Link>
+                  </div>
+</form>
+</div>
+</div>
+<div className="col-6 text-start m-0 p-0" id='img_box'></div>
+</div>
+</div>
+</Fragment>
   )
 }
 
